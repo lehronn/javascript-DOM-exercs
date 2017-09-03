27da06c9fbd77c7a1f47a1420cfc663198c5adb4
Mateusz Stomski <lehronn@gmail.com>
2017-09-03 13:57:22 +0200

    ready exercise

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //zadanie 9.5
+var paragraph = document.getElementsByTagName( 'p' );
+var buttonsArray = document.getElementsByClassName( 'button' );
+
+for (var i = 0; i < buttonsArray.length; i += 1 ) {
+  var newElem = document.createElement('p');
+  newElem.innerHTML = buttonsArray[i].innerText;
+  document.body.appendChild( newElem );
+}
